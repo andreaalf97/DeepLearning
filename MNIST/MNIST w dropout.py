16315b6d31dd068baa61cb35bec427c7d49f6c3c
adityakunar <40259197+adityakunar@users.noreply.github.com>
2020-03-09 14:21:53 +0100

    Update MNIST/MNIST w dropout.py
    Co-Authored-By: Avinash Saravanan <asarav@umich.edu>

--- MNIST/MNIST w dropout.py
+++ MNIST/MNIST w dropout.py
@@ -11,7 +11,7 @@ trainloader = torch.utils.data.DataLoader(
   batch_size=batch_size_train, shuffle=True)
 
 testloader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST(root='./data', train=False, download=True,
+  torchvision.datasets.MNIST(root='../data', train=False, download=True,
                              transform=torchvision.transforms.Compose([
                                torchvision.transforms.ToTensor(),
                                torchvision.transforms.Normalize(
@@ -93,5 +93,4 @@ for epoch in range(2):  # loop over the dataset multiple times
             running_loss = 0.0
             niter = epoch * len(trainloader) + i
 print('Finished Training')
-
 
