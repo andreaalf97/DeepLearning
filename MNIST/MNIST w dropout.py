7a198f8627cc43b40a2c53bdd159702904481f4e
Avinash Saravanan <asarav@umich.edu>
2020-03-10 09:33:11 +0100

    Get Mnist with dropout working

--- MNIST/MNIST w dropout.py
+++ MNIST/MNIST w dropout.py
@@ -1,6 +1,8 @@
 import torch
 import torchvision
 
+batch_size_train = 4
+
 trainloader = torch.utils.data.DataLoader(
   torchvision.datasets.MNIST(root='../data', train=True, download=True,
                              transform=torchvision.transforms.Compose([
@@ -9,30 +11,6 @@ trainloader = torch.utils.data.DataLoader(
                                  (0.1307,), (0.3081,))
                              ])),
   batch_size=batch_size_train, shuffle=True)
-
-testloader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST(root='../data', train=False, download=True,
-                             transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor(),
-                               torchvision.transforms.Normalize(
-                                 (0.1307,), (0.3081,))
-                             ])),
-  batch_size=batch_size_test, shuffle=True)
-
-
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
-example_data.shape
-import matplotlib.pyplot as plt
-
-fig = plt.figure()
-for i in range(6):
-  plt.subplot(2,3,i+1)
-  plt.tight_layout()
-  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-  plt.title("Ground Truth: {}".format(example_targets[i]))
-  plt.xticks([])
-  plt.yticks([])
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -44,7 +22,7 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.do = nn.Dropout(p=0.5)
-        self.do1 = nn.Dropout(p=0.8)
+        self.do1 = nn.Dropout(p=0.2)
         
         self.fc1 = nn.Linear(28*28, 1024)
         self.fc2 = nn.Linear(1024, 10)
